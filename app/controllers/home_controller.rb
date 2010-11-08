236b2ddc4ec563a8db3d471d361d4919ab65fbbb
Holger Just <web@meine-er.de>
2010-11-08 16:37:04 +0100

    Use database agnostic way for selecting random authors

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
   def index
     @extensions = Extension.find(:all, :order => "created_at DESC", :limit => 4)
-    @authors = Author.find(:all, :conditions => ["extensions_count > 0"], :order => "RAND()", :limit => 5)
+    
+    author_ids = Author.find(:all, :conditions => ["extensions_count > 0"], :select => :id).collect(&:id)
+    @authors = Author.find(:all, :conditions => {:id => author_ids.sort_by{rand}[0..5] })
   end
 end
