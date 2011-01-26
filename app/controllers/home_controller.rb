33302c6a0d4e60c8a4ecb9082d4395d70aa945d7
Holger Just <web@meine-er.de>
2011-01-26 21:47:43 +0100

    Fix typo

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @plugins = Plugins.find(:all, :order => "created_at DESC", :limit => 4)
+    @plugins = Plugin.find(:all, :order => "created_at DESC", :limit => 4)
     
     author_ids = Author.find(:all, :conditions => ["plugins_count > 0"], :select => :id).collect(&:id)
     @authors = Author.find(:all, :conditions => {:id => author_ids.sort_by{rand}[0..5] })
