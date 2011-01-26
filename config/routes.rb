cc203469cdefdcc3c6d788e9692ebec1adbec142
Holger Just <web@meine-er.de>
2011-01-26 21:48:05 +0100

    Rename extensions -> plugins in routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ ActionController::Routing::Routes.draw do |map|
   
   map.resources :authors
   map.resource  :session
-  map.resources :extensions, :collection => { :all => :get }
+  map.resources :plugins, :collection => { :all => :get }
   
   map.root :controller => "home", :action => "index"
 end
